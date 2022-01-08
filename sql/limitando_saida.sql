8654e34c4ecbbb7d54ef26b0324b72b8c9ce34d8
joaoa <joao.araujo@me.com.br>
2022-01-08 01:06:19 -0300

    Limitando saída através do LIMIT

---
+++ sql/limitando_saida.sql
@@ -0,0 +1,11 @@
+USE sucos_vendas;
+
+
+SELECT * FROM tabela_de_clientes LIMIT 20;
+
+
+SELECT DISTINCT * FROM tabela_de_clientes LIMIT 10;
+
+
+# A partir de qual e quantos após ele
+SELECT * FROM tabela_de_produtos LIMIT 2, 10;
