62e6b8bc92eea5baa44095488b8ae8d88eb3f4fe
joaoa <joao.araujo@me.com.br>
2022-01-08 01:00:51 -0300

    Uso do DISTINCT inserido

--- sql/comando_distinct.sql
+++ sql/comando_distinct.sql
@@ -1,3 +1,10 @@
 USE sucos_vendas;
 
 SELECT embalagem, tamanho FROM tabela_de_produtos;
+
+
+SELECT DISTINCT embalagem, tamanho FROM tabela_de_produtos;
+
+SELECT DISTINCT * FROM tabela_de_produtos WHERE sabor = 'Laranja';
+
+SELECT DISTINCT bairro, cidade FROM tabela_de_clientes;
