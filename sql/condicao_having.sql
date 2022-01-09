7f01ca00087bbbe3b74ce24123d5e4013b26d974
joaoa <joao.araujo@me.com.br>
2022-01-08 22:32:03 -0300

    condição having aplicada

---
+++ sql/condicao_having.sql
@@ -0,0 +1,11 @@
+USE sucos_vendas;
+
+SELECT estado, SUM(limite_de_credito) AS soma_limite FROM tabela_de_clientes
+GROUP BY estado
+HAVING SUM(limite_de_credito) > 820000;
+
+SELECT embalagem, MAX(preco_de_lista) AS maior_preco, MIN(preco_de_lista) as menor_preco FROM tabela_de_produtos
+WHERE embalagem <> 'pet'
+GROUP BY embalagem
+HAVING SUM(preco_de_lista) <= 80
+AND MAX(preco_de_lista) >= 5;
